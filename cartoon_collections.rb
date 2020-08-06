99c6c70a0f9bcd561829df8f4e011c3bc31086d7
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-06 06:28:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,15 @@
 def roll_call_dwarves(array)
   index = 0 
   array.each_with_index {|name, index|
-    puts "#{index + 1}.#{name} " }
+    puts "#{index + 1}.#{name} "}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  planteer_calls = []
+  array.collect do |item|
+    planteer_calls.push("#{item.capitalize}!")
+  end
+  planteer_calls
 end
 
 def long_planeteer_calls# code an argument here
