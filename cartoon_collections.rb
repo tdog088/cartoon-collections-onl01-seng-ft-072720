830e994d837689d242d49613e3d73c1652e7c5d7
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-06 06:29:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ def summon_captain_planet(array)
   planteer_calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  
 end
 
 def find_the_cheese# code an argument here
