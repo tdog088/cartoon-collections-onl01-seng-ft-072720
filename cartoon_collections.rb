5eaf67d4999f758154dc33f9eadb67676c4cc2f3
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-06 06:34:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types)
+  array.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
